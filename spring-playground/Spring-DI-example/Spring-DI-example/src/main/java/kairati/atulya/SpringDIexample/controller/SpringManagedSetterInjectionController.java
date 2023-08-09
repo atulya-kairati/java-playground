6a34f9884c5e0eeae9b9eddc21afaeaa48f8b95f
atulya-kairati <atulyakairati@gmail.com>
2023-08-09 15:19:12 +0530

    @Qualifier

--- spring-playground/Spring-DI-example/Spring-DI-example/src/main/java/kairati/atulya/SpringDIexample/controller/SpringManagedSetterInjectionController.java
+++ spring-playground/Spring-DI-example/Spring-DI-example/src/main/java/kairati/atulya/SpringDIexample/controller/SpringManagedSetterInjectionController.java
@@ -2,6 +2,7 @@ package kairati.atulya.SpringDIexample.controller;
 
 import kairati.atulya.SpringDIexample.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,6 +11,7 @@ public class SpringManagedSetterInjectionController {
     private GreetingService greetingService;
 
     @Autowired  // have to specify this strictly on setter if doing setter DI
+    @Qualifier("bestBean")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- spring-playground/Spring-DI-example/Spring-DI-example/src/main/java/kairati/atulya/SpringDIexample/service/GreetingServiceImpl.java
+++ spring-playground/Spring-DI-example/Spring-DI-example/src/main/java/kairati/atulya/SpringDIexample/service/GreetingServiceImpl.java
@@ -3,7 +3,7 @@ package kairati.atulya.SpringDIexample.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("bestBean")
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayHello() {
