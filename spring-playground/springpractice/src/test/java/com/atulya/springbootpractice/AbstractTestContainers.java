cef3b7b8a51ac4a836aca4ba13d4f0e572ec656c
atulya-kairati <atulyakairati@gmail.com>
2023-08-20 00:52:05 +0530

    Setting up testcontainers to test DAO

---
+++ spring-playground/springpractice/src/test/java/com/atulya/springbootpractice/AbstractTestContainers.java
@@ -0,0 +1,58 @@
+package com.atulya.springbootpractice;
+
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractTestContainers {
+
+    /**
+     * Will create a docker container with postgres image
+     * for testing
+     * It will use the given creds
+     * The container is transient and remains only for testing
+     */
+    @Container
+    public static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("dao-unit-test-db")
+                    .withUsername("atulya")
+                    .withPassword("testpass");
+
+    @DynamicPropertySource
+    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+
+        /**
+         * Since we are going to test our DAO class, and they connect to the
+         * DB using application.properties file. Therefore, won't be able to
+         * connect to our test db here, so we provide a way to override the
+         * DB creds in the application.properties that have now been loaded
+         * in memory
+         */
+
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+
+    }
+
+    @BeforeAll // runs before any of the @Test are run
+    public static void applyMigrations() { // class needs to be static since abstract classes can't have objects
+        // creating the connection to our testing db
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+
+        // applying all migrations
+        flyway.migrate();
+    }
+
+}
